4b8381778d98c25b9e3f9a44d66e4ae2a5a2e118
Psopis <konevvik05@mail.ru>
2021-12-06 15:15:47 +0700

    init4

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,8 +4,10 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.websocket.*
 import java.time.Duration
 
@@ -13,7 +15,9 @@ fun main() {
     val port = Integer.parseInt(System.getenv("PORT"))
 
     embeddedServer(Netty, port = port) {
-
+install(ContentNegotiation) {
+    json()
+}
         configureRouting()
         configureSerialization()
         websockConfigure()
