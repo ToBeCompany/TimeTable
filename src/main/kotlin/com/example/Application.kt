e6d25f5dc15b5050b26cb799e316e053bdc5a35a
Psopis <konevvik05@mail.ru>
2021-12-15 19:01:00 +0700

    init9

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -18,18 +18,13 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.Duration
 
 fun main() {
-    val port = 8080//Integer.parseInt(System.getenv("PORT"))
+    val port = Integer.parseInt(System.getenv("PORT"))
     Database.connect(
         "jdbc:postgresql://ec2-54-204-128-96.compute-1.amazonaws.com:5432/d43qcb6atqt206?sslmode=require",
         driver = "org.postgresql.Driver",
         user = "zpikpxknmoscgo",
         password ="7d2459c6a2a75d029f3ecaf7b4564bbf0114f96083a4e504886771cb212c344f"
     )
-    transaction {
-        Users.selectAll().map {
-            (UserM(it[Users.id], it[Users.name]))
-        }
-    }
 
     embeddedServer(Netty, port = port) {
 install(ContentNegotiation) {
