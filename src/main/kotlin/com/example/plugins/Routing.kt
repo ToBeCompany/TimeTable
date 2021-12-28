30ac3b4821faa1da6b9869d36c59f22d0d2784e9
Psopis <konevvik05@mail.ru>
2021-12-28 18:55:12 +0700

    init118

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -13,27 +13,27 @@ import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object marshruts :Table("marsruts"){
-    val id = integer("id")
-    val idm = integer("idm")
-    val idost = integer("idost")
-    val idostplus = integer("idostplus")
+    val id = varchar("id",20)
+    val idm = varchar("idm",20)
+    val idost = varchar("idost",20)
+    val idostplus = varchar("idostplus",20)
 }
 object marshNames :Table("marsrutnames"){
-    val id = integer("id")
+    val id = varchar("id",20)
     val name = varchar("namemarsrut",30)
 }
 object Users : Table("directory") {
 
-    val idnum = integer("idNum") // Column<String>
+    val idnum = varchar("idNum",20) // Column<String>
     val name = varchar("name", 20) // Column<String>
     val user_type = varchar("user_Type",6)
 }
 @Serializable
-data class  MarshNamesM(val id: Int = 0,val name:String = "")
+data class  MarshNamesM(val id: String = "",val name:String = "")
 @Serializable
-data class UserM(val id : Int = 0, val name : String = "", val User_type:String = "")
+data class UserM(val id : String = "", val name : String = "", val User_type:String = "")
 @Serializable
-data class MarshrutM(val id : Int, val idm_Foreign:Int,val idOst:Int,val idOstPlus:Int)
+data class MarshrutM(val id : String, val idm_Foreign:String,val idOst:String,val idOstPlus:String)
 fun Application.configureRouting() {
 
     routing {
@@ -56,8 +56,8 @@ fun Application.configureRouting() {
 
 
                 val t = Users.select(){
-                    Users.idnum eq sigin_id
-                }.find { it[Users.idnum] == sigin_id}
+                    Users.idnum eq sigin_id.toString()
+                }.find { it[Users.idnum] == sigin_id.toString()}
 
             t?.get(Users.idnum)?.let{it2 -> UserM(it2,t[Users.name],t[Users.user_type])}
           }
@@ -74,7 +74,18 @@ val a = transaction {
 }
     call.respondText(Json.encodeToString<List<MarshNamesM>>(a), ContentType.Application.Json)
 }
+        get ("/OneMarsh/{id}"){
+            val res = call.parameters["id"]?.toInt() ?: 0
+            val a = transaction {
+            val t = marshNames.select(){
+                marshNames.id eq res.toString()
+            }.find { it[marshNames.id] == res.toString()}
 
+            t?.get(marshNames.id)?.let{it2 -> MarshNamesM(it2,t[marshNames.name])}
+            }
+            call.respondText(Json.encodeToString<MarshNamesM?>(a), ContentType.Application.Json)
+
+        }
 
 
 
